cb9d9740a9e5c3ec90fc6cffc4f0be30229fe625
tmarshall <timothyjmarshall@gmail.com>
2019-12-27 16:07:12 -0800

    working on class to return redacted version of sql

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const fs = require('fs').promises
 const path = require('path')
 
-const sqlLookup = {}
-
-async function fetchSql(path) {
-  if (sqlLookup[path]) {
-    return sqlLookup[path]
-  }
-
-  const content = await fs.readFile(path, 'utf8')
-  sqlLookup[path] = content
-  return content
-}
-
 const templatized = (template, vars = {}) => {
   const handler = new Function('vars', [
     'const tagged = ( ' + Object.keys(vars).join(', ') + ' ) =>',
@@ -23,25 +11,50 @@ const templatized = (template, vars = {}) => {
   return handler(vars)
 }
 
+const literalSqlKey = Symbol('sql template with literal values')
+const redactedSqlKey = Symbol('sql template with redactions')
+class SqlTemplate {
+  constructor(template, vars) {
+    const withPublicLiterals = templatized(template, vars)
+
+    // replacing `!{}`s with `${}`s
+    // keep `\{.*\}` greedy,
+    // so any nested `!{}`s will be captured as well
+    const reconfiguredTemplate = withPublicLiterals.replace(/([^\\])!(\{.*\})/g, (_, lead, chunk) => `${lead}$${chunk}`)
+    
+    this[literalSqlKey] = templatized(reconfiguredTemplate, vars)
+    this[redactedSqlKey] = templatized(reconfiguredTemplate, Object.keys(vars).reduce((redactions, key) => {
+      redactions[key] = '[REDACTED]'
+      return redactions
+    }, {}))
+  }
+
+  toString() {
+    return this[redactedSqlKey]
+  }
+
+  inspect() {
+    return this[redactedSqlKey]
+  }
+}
+
+function dotSql(path) {
+  const template = fs.readFile(path, 'utf8')
+
+  return async function prepare(vars) {
+    return new SqlTemplate((await template), vars)
+  }
+}
+
 async function main() {
-  let sql = await fetchSql(path.resolve(__dirname, 'test.sql'))
-  
-  sql = templatized(sql, {
+  testSql = dotSql(path.resolve(__dirname, 'test.sql'))
+
+  const result = await testSql({
+    id: 123,
+    email: 'tim@%',
     limit: 100
   })
 
-  // replacing `!{}`s with `${}`s
-  // keep `\{.*\}` greedy,
-  // so any nested `!{}`s will be captured as well
-  console.log(sql)
-  sql = sql.replace(/([^\\])!(\{.*\})/g, (_, lead, chunk) => {
-    return `${lead}$${chunk}`
-  })
-  sql = templatized(sql, {
-    id: 123,
-    email: 'tim@%'
-  })
-
-  console.log(sql)
+  console.log(result)
 }
 main()
